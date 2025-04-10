30e21df62f149cdce653a06501ee2cf72e26b5dd
Akanksha <akankshashigwan0221@gmail.com>
2025-04-10 23:31:41 +0530

    user dashboard static html page coverted to react

---
+++ src/webapp/src/pages/DashboardPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import UserDashboard from '../components/dashboard/UserDashboard';
+
+const Dashboard = () => {
+  return <div>
+    <UserDashboard />
+  </div>;
+};
+
+export default Dashboard;
