bd6c8c9273037326971e834fe789ef38b0e3a0e4
shrushtidoshi22 <shrushtidoshi22@gmail.com>
2025-04-14 11:40:37 +0530

    Updated file name

--- src/webapp/src/pages/index.js
+++ src/webapp/src/pages/index.js
@@ -1,5 +1,5 @@
 export { default as HomePage } from './HomePage';
 export { default as AboutPage } from './AboutPage';
 export { default as NotFoundPage } from './NotFoundPage';
-export { default as Dashboard } from './DashboardPage';
+export { default as AdminDashboardPage } from './AdminDashboardPage';
 
